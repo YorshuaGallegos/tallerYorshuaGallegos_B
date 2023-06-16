9fdeae1308524c2e5466dd503d60c7c74b298893
YorshuaGallegos <136102789+YorshuaGallegos@users.noreply.github.com>
2023-06-16 17:39:01 +0000

    Creacion Modulo 1

---
+++ main.py
@@ -0,0 +1,11 @@
+import math 
+from Matematicas import restar,sumar,multiplicar,dividir
+from geometria import distancia
+
+print("sqrt: ", math.sqrt(25))
+print("Suma: ", sumar(2,3))
+print("Resta: ", restar(2,3))
+print("Multiplicacion: ", multiplicar(2,3))
+print("Division: ", dividir(2,3))
+print("Distancia: ", distancia(1,1,3,4))
+
